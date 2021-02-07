a8fe6cbc8423b9317579af3fa5d1444b9e30a895
KevinKhau <kkhau@hotmail.fr>
2021-02-07 18:47:16 +0100

    Drawing initial figure: outlines vertices

--- polygons.js
+++ polygons.js
@@ -4,28 +4,62 @@ import {clip, sort, getRef, getAngle} from './geometry.js';
 
 console.log(battles);
 
+const x = vertex => vertex[0] * scale;
+const y = vertex => vertex[1] * scale;
+
+/**
+ * Outlines the initial vertices in figure #1.
+ *
+ * @param context
+ * @param polygon
+ * @param strokeStyle
+ */
+function drawVertices(context, polygon, strokeStyle) {
+    context.strokeStyle = strokeStyle;
+    for (const vertex of polygon) {
+        drawCircle(context, vertex, 5, strokeStyle);
+    }
+    context.beginPath();
+    context.moveTo(polygon[0][0] * scale, polygon[0][1] * scale); //first vertex
+    for (const vertex of polygon) {
+        context.lineTo(vertex[0] * scale, vertex[1] * scale);
+    }
+    context.lineWidth = 3;
+    context.stroke();
+    context.closePath();
+}
+
 function drawPolygon(context, polygon, strokeStyle, fillStyle) {
     context.strokeStyle = strokeStyle;
-    context.fillStyle = fillStyle;
     context.beginPath();
     context.moveTo(polygon[0][0] * scale, polygon[0][1] * scale); //first vertex
     for (let i = 1; i < polygon.length; i++)
         context.lineTo(polygon[i][0] * scale, polygon[i][1] * scale);
-    context.lineTo(polygon[0][0] * scale, polygon[0][1] * scale); //back to start
-    context.fill();
+    if (fillStyle) {
+        context.lineTo(polygon[0][0] * scale, polygon[0][1] * scale); //back to start
+        context.fillStyle = fillStyle;
+        context.fill();
+    } else {
+        context.lineWidth = 5;
+    }
     context.stroke();
     context.closePath();
 }
 
 function drawRef(context, polygon) {
     const centroid = getRef(polygon);
+    drawCircle(context, centroid, 10, 'red', true);
+}
+function drawCircle(context, vertex, radius = 5, fill, stroke = false) {
     context.beginPath();
-    context.arc(centroid[0] * scale, centroid[1] * scale, 10, 0, 2 * Math.PI);
-    context.fillStyle = 'red';
+    context.arc(x(vertex), y(vertex), radius, 0, 2 * Math.PI);
+    context.fillStyle = fill;
     context.fill();
-    context.lineWidth = 2;
-    context.strokeStyle = '#003300';
-    context.stroke();
+    if (stroke) {
+        context.lineWidth = 2;
+        context.strokeStyle = '#003300';
+        context.stroke();
+    }
     context.closePath();
 }
 
@@ -50,33 +84,36 @@ function drawLines(context, polygon) {
 
 function select(battleName) {
     const {A, B} = battles[battleName];
+
     setScale(A, B);
     canvas.forEach(c => c.clearRect(-size, -size, size * 2, size * 2));
-    setGrids();
-    console.log({A, B});
-    const initialCanvas = document.querySelector('canvas.initial ').getContext("2d");
-    drawPolygon(initialCanvas, A, "#888", "#88f");
-    drawPolygon(initialCanvas, B, '#888','#8f8');
+    drawGrids();
+
+    drawVertices(initialCanvas, A, '#88f');
+    drawVertices(initialCanvas, B, '#8f8');
 
     document.querySelector('span.initial').textContent = A;
     const sortedA = sort(A), sortedB = sort(B);
 
-    const sortingCanvas = document.querySelector('canvas.sorting').getContext("2d");
     drawPolygon(sortingCanvas, B, "#888", '#8f8');
     drawRef(sortingCanvas, B);
     drawLines(sortingCanvas, B);
 
-    const resultCanvas = document.querySelector('canvas.result').getContext("2d");
-    drawPolygon(resultCanvas, A, "#888", "#88f");
-    drawRef(resultCanvas, A);
-    drawLines(resultCanvas, A);
-    drawPolygon(resultCanvas, B, '#888','#8f8');
-    drawPolygon(resultCanvas, clip(sortedA, sortedB), '#000','#0ff');
+    drawPolygon(clippingCanvas, A, "#888", "#88f");
+    drawRef(clippingCanvas, A);
+    drawLines(clippingCanvas, A);
+    drawPolygon(clippingCanvas, B, '#888','#8f8');
+    drawPolygon(clippingCanvas, clip(sortedA, sortedB), '#000','#0ff');
     document.querySelector('span.sorted').textContent = sortedA;
 }
 
 let scale = 200;
-const canvas = [...document.querySelectorAll('canvas')].map(c => c.getContext('2d'));
+const initialCanvas = document.querySelector('canvas.initial ').getContext("2d");
+const sortingCanvas = document.querySelector('canvas.sorting').getContext("2d");
+const clippingCanvas = document.querySelector('canvas.clipping').getContext("2d");
+const areaCanvas = document.querySelector('canvas.area').getContext("2d");
+
+const canvas = [initialCanvas, sortingCanvas, clippingCanvas, areaCanvas];
 const grids = [...document.querySelectorAll('canvas.grid')].map(c => c.getContext('2d'));
 const size = 400;
 
@@ -88,12 +125,13 @@ function setCanvas() {
         }
     );
 }
-function setGrids() {
+function drawGrids() {
     grids.forEach(ctx => {
         ctx.strokeStyle = 'lightgrey';
-        ctx.fontStyle = 'lightgrey';
+        ctx.fillStyle = 'blue';
         ctx.font = '12px';
         ctx.fillText('0', 2, -2);
+        ctx.lineWidth = 1;
         ctx.beginPath();
         for (let i = -size / 2; i < size / 2; i += scale) {
             ctx.moveTo(-size / 2, i);
